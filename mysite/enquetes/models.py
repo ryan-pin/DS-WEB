206b2aca0e22f137da1db27d2e39fcbf93387f6c
ryan <ryan.piinheiro@Hotmail.com>
2024-03-11 14:55:08 +0000

    criação de modelos

--- mysite/enquetes/models.py
+++ mysite/enquetes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pergunta(models.Model):
+    texto_perg = models.CharField(max_length = 200)
+    data_pub = models.DateTimeField("Data de Publicação")
+
+class Alternativa(models.Model):
+    texto_alt = models.CharField(max_length = 200)
+    qtd_votos = models.IntegerField(default = 0)
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Aplicação de enquetes - DSWEB 2024.1</h1>")
+    return HttpResponse("<h1>Aplicação de enquetes - DSWEB 2024.1</h1> <br> <h2>20231014040013</h2> <br> <h3>Ryan Pinheiro</h3>")
