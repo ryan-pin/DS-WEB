da2ff663d57cd7584c5a5f549d565c5d69fad2ac
ryan <ryan.piinheiro@Hotmail.com>
2024-03-15 13:23:10 +0000

    criação do admin

--- mysite/enquetes/models.py
+++ mysite/enquetes/models.py
@@ -1,11 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Pergunta(models.Model):
     texto_perg = models.CharField(max_length = 200)
     data_pub = models.DateTimeField("Data de Publicação")
+    def __str__(self): #overwrite
+        return "{} ({})".format(self.texto_perg, self.id)
+    def publicada_recentemente(self):
+        agora = timezone.now()
+        return self.data_pub >= agora - datetime.timedelta(hours = 24)
 
 class Alternativa(models.Model):
     texto_alt = models.CharField(max_length = 200)
-    qtd_votos = models.IntegerField(default = 0)
+    qtd_votos = models.IntegerField("Quantidade de votos", default = 0)
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    def __str__(self): #overwrite
+        return "{} ({})".format(self.texto_alt, self.id)
+
