33b11859283604abc92b7f146afe41532c2c9414
ryan <ryan.piinheiro@Hotmail.com>
2024-04-01 23:24:04 +0000

    Criação de 6 enquetes

--- mysite/enquetes/urls.py
+++ mysite/enquetes/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
 
+app_name = "enquetes"
 urlpatterns = [ #URL /enquetes/ -> lista geral das enquetes
     path("", views.index, name="index"),
-    path("<int:pergunta_id>/", views.detalhes, name="detalhes"),
+    path("<int:pergunta_id>/detalhes/", views.detalhes, name="detalhes"),
     path("<int:pergunta_id>/resultado", views.resultado, name="resultado"),
     path("<int:pergunta_id>/votacao", views.votacao, name= "votacao"),
 ]
